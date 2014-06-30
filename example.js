674eae83a41187c2b0e4650c83ceeeb14cf950b6
Rebecca Turner <turner@mikomi.org>
2014-06-30 17:34:40 +0700

    Add an example, because why not?

---
+++ example.js
@@ -0,0 +1,19 @@
+var Gearman = require('./index.js');
+var client = Gearman.Client.connect({ host:'127.0.0.1', port:4730, defaultEncoding:'utf8', debug: true, maxJobs: 2 });
+
+client.registerWorker("toUpper", function(task) {
+    task.then(function(payload) {
+        setTimeout(function(){
+            task.end(payload.toUpperCase());
+        }, 1000);
+    });
+});
+
+[1,2,3,4,5].forEach(function(v) {
+
+    client.submitJob('toUpper', {encoding: 'utf8'}, 'test string '+v,function(err,result) {
+        if (err) { throw err }
+        console.log(new Date(),"Upper:", result);
+    });
+
+});

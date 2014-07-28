7f6ec5c3d0fb34330f59bd4ef81372ead883b6dc
Rebecca Turner <turner@mikomi.org>
2014-07-28 16:20:49 -0400

    Add some very basic functional tests

---
+++ test/functional/work.js
@@ -0,0 +1,56 @@
+"use strict";
+var test = require('tape');
+var Gearman = require('./loopback');
+
+Gearman.Server.listen();
+
+test('work', function (t) {
+    t.plan(2);
+
+    var gmw = Gearman.Client.connect({packetDump: false});
+    gmw.registerWorker('upper',function (task) {
+        task.then(function(payload) {
+            task.end(payload.toUpperCase());
+        });
+    });
+    var gmc = Gearman.Client.connect({packetDump: false});
+    var message = 'test';
+    gmc.submitJob('upper',message).then(function (result) {
+        t.pass('no errors');
+        t.is(result,message.toUpperCase(),'we got uppercased');
+    })
+    .catch(function (err) {
+        t.fail('no errors');
+        process.stdout.write('# '+err);
+        t.skip();
+    })
+    .finally(function () {
+         gmw.forgetAllWorkers();
+    })
+    
+});
+
+test('error', function (t) {
+    t.plan(2);
+
+    var gmw = Gearman.Client.connect({packetDump: false});
+    gmw.registerWorker('upper',function (task) {
+        return task.then(function(payload) {
+            throw payload;
+        });
+    });
+    var gmc = Gearman.Client.connect({packetDump: false});
+    var message = 'test';
+    gmc.submitJob('upper',message).then(function (result) {
+        t.fail('errors');
+        t.skip();
+    })
+    .catch(function (err) {
+        t.pass('errors');
+        t.is(err.message,message,'got error message');
+    })
+    .finally(function () {
+        gmw.forgetAllWorkers();
+    })
+    
+});
